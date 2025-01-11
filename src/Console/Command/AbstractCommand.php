32353ce73b9d47cafb5de8d6375c2af246ee6a90
Christoph Herbst <christoph@konnektiv.de>
2025-01-11 21:07:27 +0100

    use exclusions also in commit

--- src/Console/Command/AbstractCommand.php
+++ src/Console/Command/AbstractCommand.php
@@ -3,7 +3,10 @@
 namespace PhpTuf\ComposerStagerConsole\Console\Command;
 
 use PhpTuf\ComposerStager\API\Path\Factory\PathFactoryInterface;
+use PhpTuf\ComposerStager\API\Path\Factory\PathListFactoryInterface;
 use PhpTuf\ComposerStager\API\Path\Value\PathInterface;
+use PhpTuf\ComposerStager\API\Path\Value\PathListInterface;
+use PhpTuf\ComposerStager\Internal\Path\Factory\PathListFactory;
 use PhpTuf\ComposerStagerConsole\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,7 +27,9 @@ abstract class AbstractCommand extends Command
 
     private PathInterface $stagingDir;
 
-    public function __construct(string $name, protected PathFactoryInterface $pathFactory)
+	private PathListInterface $exclusions;
+
+    public function __construct(string $name, protected PathFactoryInterface $pathFactory, private ?PathListFactoryInterface $pathListFactory = null)
     {
         parent::__construct($name);
     }
@@ -44,10 +49,30 @@ abstract class AbstractCommand extends Command
         $stagingDir = $input->getOption(Application::STAGING_DIR_OPTION);
         assert(is_string($stagingDir));
         $this->stagingDir = $this->pathFactory->create($stagingDir);
+
+	    // Define the included directories
+	    $included  = [ 'wp', 'vendor', 'wp-content' ];
+
+		if ( $this->pathListFactory ) {
+			// Get all files and directories in the active directory
+			$allEntries = scandir( $this->activeDir->absolute() );
+			// exclude wp uploads dir
+			$allEntries[] = 'wp-content/uploads';
+
+			// Filter the list to exclude the entries not in the included array
+			$this->exclusions = $this->pathListFactory->create( ...array_filter( $allEntries, function ( $entry ) use ( $included ) {
+				return ! in_array( $entry, $included, true ) && $entry !== '.' && $entry !== '..';
+			} ) );
+		}
     }
 
     protected function getActiveDir(): PathInterface
     {
         return $this->activeDir;
     }
+
+	protected function getExclusions(): PathListInterface
+	{
+		return $this->exclusions;
+	}
 }
